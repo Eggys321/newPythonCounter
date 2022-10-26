0e48e3686bae6cd51eceacf4b3b00a8ab5c6c5a3
Eggys321 <imonieroheguono@gmail.com>
2022-10-26 14:30:42 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+let heading = document.querySelector('h1');
+let inc = document.querySelector('.inc')
+let res = document.querySelector('.res')
+let dec = document.querySelector('.dec')
+
+// DECREASE
+function DECREASE (){
+    heading.innerText--
+}
+
+dec.addEventListener('click', DECREASE)
+// RESET
+function RESET (){
+    heading.innerText = 0
+}
+
+res.addEventListener('click', RESET)
+// INCREASE
+function INCREASE (){
+    heading.innerText++
+}
+
+inc.addEventListener('click', INCREASE)
